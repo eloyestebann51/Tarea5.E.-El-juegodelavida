7ee2aa7afd8a8b49dcc2654d5d6887ca11018044
eloyestebann51 <eloymax51@gmail.com>
2025-03-19 14:30:00 +0100

    primercommit

---
+++ src/main/java/daw/Tarea5EElJuegodelavida.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+/**
+ *
+ * @author eloy
+ */
+public class Tarea5EElJuegodelavida {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
