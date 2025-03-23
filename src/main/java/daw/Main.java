34461a7484f8c49013914eb872bb1959633eec2a
eloyestebann51 <eloymax51@gmail.com>
2025-03-24 00:19:36 +0100

    Actualizacion posibles errores

--- src/main/java/daw/Main.java
+++ src/main/java/daw/Main.java
@@ -7,7 +7,19 @@ package daw;
  */
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Eloy");
+public static void main(String[] args) {
+        int tamaño = 10;
+        double porcentajeVivas = 30.0;
+
+        Tablero tablero = new Tablero(tamaño, porcentajeVivas);
+        System.out.println("Generacion Inicial:");
+        tablero.mostrarTablero();
+
+        int generaciones = 5;
+        for (int i = 1; i <= generaciones; i++) {
+            tablero.generarSiguienteGeneracion();
+            System.out.println("Generacion " + i + ":");
+            tablero.mostrarTablero();
+        }
     }
 }
